9d92b8c08cf5d58c2e950040d17c8dede49226fd
NATER <165908372+kurapicu@users.noreply.github.com>
2025-07-23 12:57:39 +0100

    first commit

---
+++ operators.py
@@ -0,0 +1,43 @@
+# Arithmetic Operators
+
+# addition -- +
+
+num = 12
+num2 = 14
+
+my_sum = num +num2
+print(my_sum)
+
+
+# subtraction -- -
+
+my_subtraction = num2 - num
+print(my_subtraction)
+
+# multiplication -- *
+
+my_multiplication = num * num2
+print(my_multiplication)
+
+# division -- /
+
+my_division = num2 / num
+print(my_division)
+
+# floor division -- //
+
+my_floor_division = num2 // num
+print(my_floor_division)
+
+# modulus -- %
+
+my_modulus = num2 % num
+print(my_modulus)
+
+# exponentiation -- **
+my_exponentiation = num ** 2
+print(my_exponentiation)    
+
+#Comparison operators
+
+#Equal to --- 
